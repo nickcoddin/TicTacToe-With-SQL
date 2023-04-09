acae5bca381e81d2e691c7f6a17edd42fc7b7f80
PC <nikoloz.de@gmail.com>
2023-04-09 14:49:48 +0400

    i have change Game Object

--- NewTicTacToeWithSQL/DataAccess/CreateDbInfo.cs
+++ NewTicTacToeWithSQL/DataAccess/CreateDbInfo.cs
@@ -2,10 +2,8 @@
 
 namespace NewTicTacToeWithSQL.DataAccess
 {
-    internal class CreateDbInfo
+     internal class CreateDbInfo
     {
-        //Game game = new Game();
-
         //Method 1: Adds time in database!
         public void AddGame(Game game)
         {
